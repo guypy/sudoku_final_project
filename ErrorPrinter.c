9e13a755ef46a7a27f003f74b636e70caee2c4b1
yuvalbvia <yuval.barzam@ridewithvia.com>
2018-08-24 14:26:36 +0300

    added error messages (#1)

--- ErrorPrinter.c
+++ ErrorPrinter.c
@@ -4,3 +4,59 @@
 void errPrinter_invalidCommand(){
     printf("ERROR: invalid command\n");
 }
+
+void errPrinter_erroneousValues(){
+    printf("Error: board contains erroneous values\n");
+}
+
+void errPrinter_valueNotInRange(int n){
+    printf("Error: value not in range 0-%d \n", n);
+}
+
+void errPrinter_fixedCell(){
+    printf("Error: cell is fixed\n");
+}
+
+void errPrinter_unsolvableBoard(){
+    printf("Error: board is unsolvable\n");
+}
+
+void errPrinter_cellContainsValue(){
+    printf("Error: cell already contains a value\n");
+}
+
+void errPrinter_cannotCreateOrModifyFile(){
+    printf("Error: File cannot be created or modified\n");
+}
+
+void errPrinter_cannotOpenFile(){
+    printf("Error: File cannot be opened\n");
+}
+
+void errPrinter_fileDoesNotExist(){
+    printf("Error: File doesn’t exist or cannot be opened\n");
+}
+
+void errPrinter_boardValidationFailed(){
+    printf("Error: board validation failed\n");
+}
+
+void errPrinter_noMovesToRedo(){
+    printf("Error: no moves to redo\n");
+}
+
+void errPrinter_noMovesToUndo(){
+    printf("Error: no moves to undo\n");
+}
+
+void errPrinter_puzzleGeneratorFailed(){
+    printf("Error: puzzle generator failed\n");
+}
+
+void errPrinter_boardNotEmpty(){
+    printf("Error: board is not empty\n");
+}
+
+void errPrinter_valueIsNotZeroOrOne(){
+    printf("Error: the value should be 0 or 1\n");
+}
--- ErrorPrinter.h
+++ ErrorPrinter.h
@@ -1,5 +1,19 @@
 #ifndef SUDOKU_CONSOLE_ERRORPRINTER_H
 #define SUDOKU_CONSOLE_ERRORPRINTER_H
 void errPrinter_invalidCommand();
+void errPrinter_erroneousValues();
+void errPrinter_valueNotInRange(int n);
+void errPrinter_fixedCell();
+void errPrinter_unsolvableBoard();
+void errPrinter_cellContainsValue();
+void errPrinter_cannotCreateOrModifyFile();
+void errPrinter_cannotOpenFile();
+void errPrinter_fileDoesNotExist();
+void errPrinter_boardValidationFailed();
+void errPrinter_noMovesToRedo();
+void errPrinter_noMovesToUndo();
+void errPrinter_puzzleGeneratorFailed();
+void errPrinter_boardNotEmpty();
+void errPrinter_valueIsNotZeroOrOne();
 
 #endif //SUDOKU_CONSOLE_ERRORPRINTER_H
